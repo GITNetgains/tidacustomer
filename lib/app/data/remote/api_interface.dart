810d6fb2bbdec05183030e11e6bcb83ce1cd32db
K Tejeshwar Reddy <lci2020055@iiitl.ac.in>
2023-11-06 09:54:15 +0530

    update on notifications for ios

--- lib/app/data/remote/api_interface.dart
+++ lib/app/data/remote/api_interface.dart
@@ -1,6 +1,7 @@
 abstract class ApiInterface {
   static const baseUrl = "https://tidasports.com/secure/api";
-  static const notificationServiceUrl = "http://localhost:5001";
+  static const notificationServiceUrl =
+      "https://tida-notification-service.onrender.com";
 
   Future postApi({
     String? url,
